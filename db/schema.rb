c42c9ac763ea69e8d75313a3c50dc7e93732fce7
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2015-11-09 19:29:42 +0600

    Add Answer model and has_many assoc

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109130157) do
+ActiveRecord::Schema.define(version: 20151109132424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "question_id"
+    t.text     "body"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 20151109130157) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+
+  it { should have_many :answers }
 end
