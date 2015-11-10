039ac3670d80002a91573733d9a374cff15fbcbc
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2015-11-10 12:08:11 +0600

    add Questions#show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,8 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET :index' do
-    let(:questions) { FactoryGirl.create_list(:question, 2) }
+    let(:questions) { create_list(:question, 2) }
     before { get :index }
 
     it 'should have questions array' do
@@ -10,6 +12,16 @@ RSpec.describe QuestionsController, type: :controller do
     end
     it 'should render :index template' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe "GET :show" do
+    before { get :show, id: question }
+    it 'should have right question' do
+      expect(assigns(:question)).to eq question
+    end
+    it 'should render :show template' do
+      expect(response).to render_template :show
     end
   end
 end
