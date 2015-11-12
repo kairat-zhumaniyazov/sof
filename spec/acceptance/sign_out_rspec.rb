10f017227848dd56b97dfbaffb486671ffe01a75
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2015-11-12 13:12:14 +0600

    + Acceptance tests Sign Up and Sign out

---
+++ spec/acceptance/sign_out_rspec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User sign in', %q{
+  In order to be able to close session
+  As an Signed in User
+  I want to be able to sign out
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'Signed in user try to sign out' do
+    sign_in(user)
+
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq root_path
+  end
+end
