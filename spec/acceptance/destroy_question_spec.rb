ffc627216d4e1b8d73b28033f4f26ff7fba0cfb2
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2015-11-13 10:40:40 +0600

    * Delete question link for non-signed in and not owner

--- spec/acceptance/destroy_question_spec.rb
+++ spec/acceptance/destroy_question_spec.rb
@@ -18,18 +18,14 @@ feature 'Destroy Question', %q{
     expect(page).to have_content 'Your question deleted.'
   end
 
-  scenario 'Non-sign in user can not destroy question' do
+  scenario 'Non-sign in user dont have delete question link' do
     visit question_path question
-    click_on 'Delete question'
-
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    expect(page).to_not have_link 'Delete question'
   end
 
-  scenario 'Not owner can not destroy question' do
+  scenario 'Not owner dont have delete question link' do
     sign_in user
     visit question_path another_question
-    click_on 'Delete question'
-
-    expect(page).to have_content 'You can not delete this question.'
+    expect(page).to_not have_link 'Delete question'
   end
 end
