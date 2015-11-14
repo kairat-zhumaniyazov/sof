2284b07ecc1fa2c788438f63ba8c14bcd3de3f75
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2015-11-14 17:06:06 +0600

    + Ajax for Answers#create

--- spec/acceptance/create_answer_spec.rb
+++ spec/acceptance/create_answer_spec.rb
@@ -9,7 +9,7 @@ feature 'Create Answer for Question', %q{
   given(:user) { create(:user) }
   given(:question) { create(:question) }
 
-  scenario 'Signed in user try to create answer' do
+  scenario 'Signed in user try to create answer', js: true do
     sign_in user
     visit question_path question
 
@@ -19,10 +19,11 @@ feature 'Create Answer for Question', %q{
     within 'div.answers' do
       expect(page).to have_content 'My Answer'
     end
+    save_and_open_page
     expect(current_path).to eq question_path(question)
   end
 
-  scenario 'Signed in user try to create answer with invalid params' do
+  scenario 'Signed in user try to create answer with invalid params', js: true do
     sign_in user
     visit question_path question
 
@@ -36,7 +37,7 @@ feature 'Create Answer for Question', %q{
 
     fill_in 'Answer body', with: 'My Answer'
     click_on 'Create Answer'
-
+    save_and_open_page
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
