b8a8b869c87cf07ce719d5dc744d740e3c545a0e
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2015-11-15 18:56:48 +0600

    + Ajax for Questions#edit

--- app/assets/javascripts/questions.coffee
+++ app/assets/javascripts/questions.coffee
@@ -1,6 +1,15 @@
 $ ->
+  $('.question').on 'click', 'a.edit-question-link', (e) ->
+    e.preventDefault()
+    alert('gogo')
+    $(this).hide()
+    $('div#edit-question-form-container').show()
+    return
+
+$ ->
   $('.answers').on 'click', 'a.edit-answer-link', (e) ->
     e.preventDefault()
     answer_id = $(this).data('answer-id')
     $(this).hide()
     $('form#edit_answer_' + answer_id).show()
+    return
---
+++ spec/acceptance/question/edit_question_spec.rb
@@ -0,0 +1,47 @@
+require_relative '../acceptance_helper'
+
+feature 'Edit Question', %q{
+  In order to edit  question
+  For Question Author
+  I want to edit question
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question, user: user) }
+  given(:another_user) { create(:user) }
+
+  describe 'Author' do
+    before do
+      sign_in user
+      visit question_path question
+    end
+
+    scenario 'have edit link' do
+      expect(page).to have_link 'Edit'
+    end
+
+    scenario 'can edit question', js: true do
+      click_on 'Edit'
+      fill_in 'Title', with: 'Edited title'
+      fill_in 'Body', with: 'Edited body'
+      click_on 'Save'
+
+      expect(page).to_not have_content question.title
+      expect(page).to_not have_content question.body
+
+      expect(page).to have_content 'Edited title'
+      expect(page).to have_content 'Edited body'
+    end
+  end
+
+  scenario 'Not an author dont have edit link' do
+    sign_in another_user
+    visit question_path question
+    expect(page).to_not have_link 'Edit'
+  end
+
+  scenario 'Non-authed user dont have edit link' do
+    visit question_path question
+    expect(page).to_not have_link 'Edit'
+  end
+end
