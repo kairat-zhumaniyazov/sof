75179f3d3665603efe4159f5e7e0bf779e3e16f0
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2015-11-18 15:46:02 +0600

    + shared partial; + some fix

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Question, type: :model do
 
   it { should have_many(:answers).dependent(:destroy) }
   it { should belong_to(:user) }
-  it { should have_many(:attachments), as: :attachable }
+  it { should have_many(:attachments) }
 
   it { should accept_nested_attributes_for :attachments }
 end
