4c2a5662cc95fc352f8de58fd17f6d84d2a67cf7
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2015-12-12 22:47:49 +0600

    + doorkeeper for only admin; * some fix

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ProfilesController < ApplicationController
   before_action :doorkeeper_authorize!
   #skip_authorization_check
-  authorize_resource User, user: current_resource_owner
+  authorize_resource User, user: @current_resource_owner
 
   respond_to :json
 
