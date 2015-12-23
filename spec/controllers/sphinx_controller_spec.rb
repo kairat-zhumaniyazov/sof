63bc7a6ac60e52ee08ca36c7a523afb74e4cc39b
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2015-12-23 13:40:51 +0600

    * convert Kernel.const_get to safe_constantize #2

--- spec/controllers/sphinx_controller_spec.rb
+++ spec/controllers/sphinx_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SphinxController, type: :controller do
   describe 'GET #search' do
     [nil, 'question', 'answer', 'comment', 'user'].each do |index|
       it "with index: #{index} should receive to right indexed object" do
-        obj = index ? Kernel.const_get(index.capitalize) : ThinkingSphinx
+        obj = index ? index.capitalize.constantize : ThinkingSphinx
         expect(obj).to receive(:search).with(kind_of(String))
         get :search, search_query: { q: 'search query', index: index }
       end
