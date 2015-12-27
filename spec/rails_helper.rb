4d7f10c13a1b236be3cace30fd32f2c178884f5a
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2015-12-27 21:36:27 +0600

    + tests focus filter

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,6 +61,7 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
+  config.filter_rails_from_backtrace!
 end
 
 Shoulda::Matchers.configure do |config|
