b8aeda74dd9b2c88760a6a3f73d75d53cc5a5f2a
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2016-01-23 14:40:56 +0600

    * default scope ->

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,7 +8,7 @@ class Answer < ActiveRecord::Base
 
   validates :body, :question_id, :user_id, presence: true
 
-  default_scope -> { order(best: :desc).order(created_at: :asc) }
+  default_scope { order(best: :desc).order(created_at: :asc) }
 
   after_create :new_answer_notification
   after_create :calculate_reputation
