34e9d04c4d2c45d476ac24e93a2a72d840386e75
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2016-02-19 00:59:45 +0600

    + hashtag links

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,7 +1,11 @@
 module QuestionsHelper
   def hashtags_to_links(text)
     text
-      .gsub(Question::HASHTAG_REGEXP_PATTERN, link_to('#\1', root_path))
+      .gsub(Question::HASHTAG_REGEXP_PATTERN) { hashtag_link(Regexp.last_match(1)) }
       .html_safe
+  end
+
+  def hashtag_link(hashtag)
+    link_to("##{hashtag}", tagged_questions_path(hashtag))
   end
 end
