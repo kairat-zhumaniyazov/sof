fa01d8b90dafa120482c00eac3754468c0cd6310
Kairat Zhumaniyazov <k.zhumaniyazov@gmail.com>
2016-02-19 12:45:02 +0600

    * SphinxController reworked

--- app/controllers/sphinx_controller.rb
+++ app/controllers/sphinx_controller.rb
@@ -1,21 +1,37 @@
 class SphinxController < ApplicationController
-  before_action :indexed_class, only: :search
+  before_action :query_processing, only: :search
 
   def search
     authorize! :read, @results
-    respond_with @results = @index.search(search_params[:q])
+    respond_with @results
   end
 
   private
 
-  # rubocop:disable Metrics/LineLength
-  def indexed_class
+  def query_processing
     param_index = search_params[:index]
-    @index =
-      SearchQuery::INDICES.include?(param_index) ? param_index.capitalize.safe_constantize : ThinkingSphinx
+    if SearchQuery::INDICES.include?(param_index)
+      send("search_#{param_index}".to_sym,
+           search_params[:q], param_index.capitalize.safe_constantize)
+    else
+      send(:search_everywhere, search_params[:q])
+    end
   end
 
   def search_params
     params.require(:search_query).permit(:q, :index)
   end
+
+  def search_everywhere(query)
+    @results = ThinkingSphinx.search(query)
+  end
+
+  def search_model(query, klass = nil)
+    @results = klass.search(query) if klass
+  end
+
+  alias_method :search_question, :search_model
+  alias_method :search_answer, :search_model
+  alias_method :search_comment, :search_model
+  alias_method :search_user, :search_model
 end
